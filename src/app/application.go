ad100668a7e257ba89641aaf06f90e74aa74522d
posol <dmdmasusapjira1@gmail.com>
2020-05-30 13:09:41 +0500

    add cassandra client

--- src/app/application.go
+++ src/app/application.go
@@ -2,9 +2,11 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/posol/bookstore_oauth-api/clients/cassandra"
 	"github.com/posol/bookstore_oauth-api/src/domain/access_token"
 	"github.com/posol/bookstore_oauth-api/src/http"
 	"github.com/posol/bookstore_oauth-api/src/repository/db"
+	"log"
 )
 
 var (
@@ -12,6 +14,12 @@ var (
 )
 
 func StartApplication() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		log.Fatal(dbErr)
+	}
+	session.Close()
+
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/api/oauth/access_token/:access_token_id", atHandler.GetById)
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,14 +1,22 @@
 package db
 
 import (
+	"github.com/posol/bookstore_oauth-api/clients/cassandra"
 	"github.com/posol/bookstore_oauth-api/src/domain/access_token"
 	"github.com/posol/bookstore_oauth-api/src/utils/errors"
+	"log"
 )
 
 type dbRepository struct {
 }
 
 func (r dbRepository) GetById(accessTokenId string) (*access_token.AccessToken, *errors.RestError) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
 	return nil, errors.NewIntrenalServerError("database connection not implemented yet")
 }
 
